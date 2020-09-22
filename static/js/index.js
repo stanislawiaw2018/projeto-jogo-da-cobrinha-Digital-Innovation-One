3eaf5890bdb2e9633b31ab81a64794828ce033d5
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2020-09-21 21:33:46 -0300

    defined direction and delimited the limits for the snake not short

--- static/js/index.js
+++ static/js/index.js
@@ -7,13 +7,13 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = "rigth";
+let direction = "right";
 
 //Resposavel pela criação e construção do background do tabuleiro do jogo
 function createBG() {
     context.fillStyle = "lightgreen";
     //responsavel por desenhar o tabuleiro
-    context.fillRect(0,0,16 * box, 16 * box);
+    context.fillRect(0 , 0 , 16 * box , 16 * box);
 }
 
 //Responsavel pela criação e redenrização da cobrinha em tela
@@ -24,7 +24,23 @@ function createSnake(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+
+}
+
 function startGame(){
+    //Define a cobrinha a atravessar a parede
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
 
     createBG();
     createSnake();
@@ -35,8 +51,8 @@ function startGame(){
 
     if(direction == "right") snakeX +=box;
     if(direction == "left") snakeX -=box;
-    if(direction == "up") snakeX -=box;
-    if(direction == "down") snakeX +=box;
+    if(direction == "up") snakeY -=box;
+    if(direction == "down") snakeY +=box;
 
     snake.pop();
 
